8f5d8aee79b76eb197753f23ff74aa5bb75ad42b
AnggreinyRolangon <anggreinyangelia30@gmail.com>
2021-02-17 15:27:25 -0800

    excer

---
+++ pertemuan5/index.js
@@ -0,0 +1,19 @@
+let numbers = [1, 2, 3, 4, 5];
+
+//for(let i =0; i <= numbers, length; i++){
+  //  console.log(numbers[1]);
+//}
+//numbers.forEach(function(number, index, arr){
+ //   number *= 2;
+   // console.log('value'+ number +'index:'+ index + "Array :"+ arr);
+//});
+
+//numbers.forEach(function(number){
+  //  console.log(numbers *2);
+//});
+///et output = numbers.map(function(number){
+   // return (number *=2);
+//})
+
+//console.log("Array numbers", numbers);
+//console.log("Array numbers", output);
